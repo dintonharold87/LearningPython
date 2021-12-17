914e50e4c9f0d97a37d6c7c88405e775f8790b8a
dinton harold <dintonharold@gmail.com>
2021-12-18 00:06:54 +0300

    Day 5 assignment about a python script that adds key value pairs to a dictionary

--- day4.py
+++ day4.py
@@ -28,7 +28,47 @@ print(set(guests))
 #pop function removes the last item  while remove function removes a specific item
 guests.remove("Ainemukama")
 print(guests)
-
 #del deletes a specific range of items from the list
 del num2[2:3]
 print(num2)
+
+
+#TUPLES- are identified using a pair of parentheses
+""" A tuple is immutable ie it can't be changed 
+it can't be manipulated, it can only be read """
+myTuple = (1,4,5,3,88,7,5,455)
+myList=[]
+myList.append(myTuple,)
+myList.append([1,2,3,4])
+print(myList)
+myList.insert(0,5)
+myList.extend([3])
+print(myList)
+
+
+
+""" A DICTIONARY- this is identified using a pair of braces
+syntax-{key:value} """
+personX={"name":"Dinton","DOB":1997}
+print(personX["name"])
+
+my_box={}
+for x in range(7):
+    my_box[x]= x*x
+print(my_box)
+#this checks if the key 1 is in the dictionary called my_box(the answer is a boolen value either true or false)
+print(1 in my_box)
+
+#this is to print all value in dictionary
+for i in my_box:
+    print(my_box[i])
+# this is to print all the keys in the dictionary
+for i in my_box:
+    print(i)
+# this is to print the entire dictionary with keys and their values
+for i in my_box:
+    print(i,my_box[i])
+#this is to print values of a dictionary that are even
+for i in my_box:
+    if my_box[i] % 2 == 0:
+        print(i,my_box[i])
---
+++ day5assignment.py
@@ -0,0 +1,7 @@
+myDictionary={}
+values = ["Dinton", "male", "Uganda", "Single"]
+keys = ["name", "sex", "country of origin", "marital status"]
+for i in range(len(keys)):
+    myDictionary[keys[i]]=values[i]
+    
+print(myDictionary)
