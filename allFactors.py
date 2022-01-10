d6b0e6766e3cb70df475070d06b92685d148e47e
dinton harold <dintonharold@gmail.com>
2022-01-10 15:33:02 +0300

    Python script that gets factors of a number and returns only the prime numbers

---
+++ allFactors.py
@@ -0,0 +1,34 @@
+def allFactors(number):
+    
+    #write your function here
+    allFactorList = []
+    number=int(number)
+    if number>1:
+        for i in range(1,number+1):
+            if(number%i)==0:
+                allFactorList.append(i)
+                continue
+        else:
+                
+            return(allFactorList)
+    else:
+        return -1
+# allFactors(300)  
+
+def primeFactors(number):
+    #write your function here
+    factorlist=allFactors(number)
+    
+    primeList=[]
+    for i in factorlist:
+        if i==1:continue
+        flag=True
+        for j in range(2,i):
+            if i%j==0:
+                flag=False
+        if flag==True:
+            primeList.append(i)
+    print(primeList)
+    
+primeFactors(300)
+    
