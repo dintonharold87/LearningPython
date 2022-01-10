ce682fc1154e73bb75c5e952a3638d8c97ce761f
dinton harold <dintonharold@gmail.com>
2022-01-10 09:48:33 +0300

    Data analysis of a csv file

--- dataAnalysis1.py
+++ dataAnalysis1.py
@@ -1,3 +1,36 @@
+""" Series is a one-dimensional labeled array capable of holding data of any type (integer, string, float, python objects, etc.). The axis labels are collectively called index.
+
+pandas.Series
+A pandas Series can be created using the following constructor −
+pandas.Series( data, index, dtype, copy)
+
+The parameters of the constructor are as follows −
+1
+data
+
+data takes various forms like ndarray, list, constants
+
+2	
+index
+
+Index values must be unique and hashable, same length as data. Default np.arrange(n) if no index is passed.
+
+3	
+dtype
+
+dtype is for data type. If None, data type will be inferred
+
+4	
+copy
+
+Copy data. Default False 
+
+A series can be created using various inputs like −
+
+Array
+Dict
+Scalar value or constant"""
+#import the pandas library and aliasing as pd
 import pandas as pd
 # Converting a list into a data serie
 first_list=[10,20,30,40]
@@ -15,3 +48,4 @@ fruits={'apples':10,'bananas':20,'grapes':30,'pineapples':40,'watermelons':50,'k
 third_series=pd.Series(fruits)
 print('Fruits at home \n',third_series)
 print('We have',third_series['pineapples'],'pineapples at home')
+
